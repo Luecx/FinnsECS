f55fe340d48aa7c95e11a3cb1edab74d92aaabd0
Finn Eggers <finn@Finns-MacBook-Pro.local>
2024-06-22 21:59:23 +0200

    update

--- src/include.h
+++ src/include.h
@@ -5,12 +5,12 @@
 #ifndef ECS_ECS_CORE_H_
 #define ECS_ECS_CORE_H_
 
+#include "types.h"
+#include "hash.h"
 #include "component.h"
 #include "ecs.h"
 #include "entity.h"
 #include "event.h"
-#include "hash.h"
 #include "system.h"
-#include "types.h"
 
 #endif    // ECS_ECS_CORE_H_
